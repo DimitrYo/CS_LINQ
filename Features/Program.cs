e4200da6c469e7857bf508928dc3844e9dd6f9f0
Dymytr Yovchev <Dymytr.Yovchev@simcorp.com>
2018-09-24 10:36:48 +0300

    LINQ base.

---
+++ Features/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//using Features.Linq;
+
+namespace Features {
+    class Program {
+        static void Main(string[] args) {
+
+            IEnumerable<Employee> developers = new Employee[] {
+                new Employee { Id = 1, Name = "Scott"},
+                new Employee { Id = 2, Name = "Chris"}
+            };
+
+            IEnumerable<Employee> sales = new List<Employee>() {
+                new Employee { Id = 3, Name = "Alex"}
+            };
+
+            //foreach (var person in sales) {
+            //    Console.WriteLine(person.Name);
+            //}
+
+            //Console.WriteLine(sales.Count());
+            //Console.WriteLine(
+
+            //var enumerator = sales.GetEnumerator();
+            //while (enumerator.MoveNext()) {
+            //    Console.WriteLine(enumerator.Current.Name);
+
+            //}
+
+            //foreach (var employee in developers.Where(NameStartsWithS)) {
+            //    Console.WriteLine(employee.Name);
+            //}
+            //Console.ReadLine();
+
+            //foreach (var employee in developers.Where(
+            //    delegate (Employee employee) {
+            //        return employee.Name.StartsWith("S");})) {
+            //    Console.WriteLine(employee.Name);
+            //}
+            //Console.ReadLine();
+
+            Func<int, int> f = Square;
+            Console.WriteLine(f(2));
+            Func<int, int> square = x => x * x;
+            Console.WriteLine(square(2));
+            Func<int, int, int> add = (x, y) => x + y;
+            Console.WriteLine(add(2, 3));
+
+            Action<int> write = x => Console.WriteLine(x);
+            write(square(add(3, 5)));
+
+            //foreach (var employee in developers.Where(e => e.Name.StartsWith("S"))) {
+            //    Console.WriteLine(employee.Name);
+            //}
+
+            foreach (var employee in developers
+                .Where(e => e.Name.Length == 5)
+                .OrderBy(e => e.Name)
+                ) {
+                Console.WriteLine(employee.Name);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static int Square(int x) {
+            return x * x;
+        }
+
+        private static bool NameStartsWithS(Employee employee) {
+            return employee.Name.StartsWith("S");
+        }
+    }
+}
--- Introduction/Program.cs
+++ Introduction/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,39 @@ using System.Threading.Tasks;
 namespace Introduction {
     class Program {
         static void Main(string[] args) {
+            string path = @"C:\windows";
+            ShowLargeFilesWithoutLinq(path);
+            Console.WriteLine("***");
+            ShowLargeFilesWithLinq(path);
+        }
+
+        private static void ShowLargeFilesWithLinq(string path) {
+            var query = from file in new DirectoryInfo(path).GetFiles()
+                        orderby file.Length descending
+                        select file;
+
+            //var query = new DirectoryInfo(path).GetFiles()
+            //    .OrderByDescending(f => f.Length)
+            //    .Take(5);
+
+            foreach (var file in query.Take(5)) {
+                Console.WriteLine($"{file.Name,-25} : {file.Length,20:NO}");
+            }
+        }
+
+        private static void ShowLargeFilesWithoutLinq(string path) {
+            DirectoryInfo directory = new DirectoryInfo(path);
+            var files = directory.GetFiles();
+            Array.Sort(files,new FileInfoComparer());
+
+            foreach (var file in files) {
+                Console.WriteLine($"{file.Name, -25} : {file.Length, 20:NO}");
+            }
+        }
+    }
+    public class FileInfoComparer : IComparer<FileInfo> {
+        public int Compare(FileInfo x, FileInfo y) {
+            return y.Length.CompareTo(x.Length);
         }
     }
 }
